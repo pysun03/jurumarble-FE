42cee6914b08f88f1d690409d4785569c4678c17
Kim Minjun <swmjoon@gmail.com>
2023-08-26 23:06:04 +0900

    Refactor: setting 코드 리뷰

--- select/asset/script.js
+++ select/asset/script.js
@@ -1,13 +1,28 @@
-windwow.onload = () => howmany(8) 
-function howmany(n)
-{
-    n=8;
-    const container = document.querySelector('.container')[0];
-    for(let i=1;i<n+1;i++) {
-        const img = document.createElement("img");
-        img.src = `cap_$(i).png`;
-        container.appendChild(img);
-    }
-    
-}
+window.onload = () => howmany(6);
 
+function howmany(n) {
+  const container = document.querySelector(".container");
+  let str = "";
+  for (let i = 1; i < n + 1; i++) {
+    // const div = document.createElement("div");
+
+    // const img = document.createElement("img");
+    // img.src = `./static/cap_${i}.png`;
+
+    // const input = document.createElement("input");
+    // input.type = "text";
+
+    // div.appendChild(img);
+    // div.appendChild(input);
+
+    // container.appendChild(div);
+
+    str += `
+        <div class="user">
+            <img src="./static/cap_${i}.png" />
+            <input type="text"/>
+        </div>
+    `;
+  }
+  container.innerHTML = str;
+}
