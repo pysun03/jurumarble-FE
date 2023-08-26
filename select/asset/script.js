3e35fec57a47c8f092e5e8027f61a78662517aa9
franky0201 <franky0201@naver.com>
2023-08-26 22:43:05 +0900

    fix: all changed
    help

--- select/asset/script.js
+++ select/asset/script.js
@@ -1,1 +1,13 @@
-const N = 4;
+windwow.onload = () => howmany(8) 
+function howmany(n)
+{
+    n=8;
+    const container = document.querySelector('.container')[0];
+    for(let i=1;i<n+1;i++) {
+        const img = document.createElement("img");
+        img.src = `cap_$(i).png`;
+        container.appendChild(img);
+    }
+    
+}
+
