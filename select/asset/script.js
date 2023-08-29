e3166745a90e77156a57167313af4bd35e275b2e
franky0201 <franky0201@naver.com>
2023-08-29 15:23:28 +0900

    docs: design changed

--- select/asset/script.js
+++ select/asset/script.js
@@ -1,4 +1,4 @@
-window.onload = () => howmany(6);
+window.onload = () => howmany(8);
 
 function howmany(n) {
   const container = document.querySelector(".container");
@@ -20,7 +20,7 @@ function howmany(n) {
     str += `
         <div class="user">
             <img src="./static/cap_${i}.png" />
-            <input type="text"/>
+            <input type="text" placeholder="플레이어명(팀명)"/>
         </div>
     `;
   }
