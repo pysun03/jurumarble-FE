02581d5b0ddce6bd11cf4c6107c1df8d7dfef6fc
franky0201 <franky0201@naver.com>
2023-10-05 17:13:52 +0900

    Update script.js

--- select/asset/script.js
+++ select/asset/script.js
@@ -3,12 +3,12 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const userCount = urlParams.get('userCount');
 if (userCount !== null && !isNaN(userCount)) {
-    howmany(userCount*1) } 
+    howmany(userCount*1) } ;
   }
 
 function howmany(n) {
   const container = document.querySelector(".container");
-  let str = "";
+  let str = ""
   for (let i = 1; i < n + 1; i++) {
     // const div = document.createElement("div");
 
