50acdc2132fafd8cf28a9870bfeb2e3e903deb99
Kim Minjun <60740447+mjun0328@users.noreply.github.com>
2023-10-05 17:30:17 +0900

    Fix: window.onload 오류 해결

--- select/asset/script.js
+++ select/asset/script.js
@@ -1,14 +1,15 @@
-window.onload() = function(){
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const userCount = urlParams.get('userCount');
-if (userCount !== null && !isNaN(userCount)) {
-    howmany(userCount*1) } ;
+window.onload = function () {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const userCount = urlParams.get("userCount");
+  if (userCount !== null && !isNaN(userCount)) {
+    howmany(userCount * 1);
   }
+};
 
 function howmany(n) {
   const container = document.querySelector(".container");
-  let str = ""
+  let str = "";
   for (let i = 1; i < n + 1; i++) {
     // const div = document.createElement("div");
 
