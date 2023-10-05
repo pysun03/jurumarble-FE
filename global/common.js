de783f123707f2fd9a3b39641d8659f8b2eb1572
pepcsy <jjhjsy7@gmail.com>
2023-10-05 17:13:22 +0900

    feat: play 화면 사이드바 만들기, 배경음악 소리크기 쿠키값으로 생성

--- global/common.js
+++ global/common.js
@@ -1,21 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const bg = document.getElementById('bg');
-    const backgroundMusic = document.getElementById('backgroundMusic');
     const se = document.getElementById('se');
-    const soundEffects = document.getElementById('soundEffects');
     const buttons = document.querySelectorAll('button');
-
-    // 배경 음악 볼륨 조절
-    backgroundMusic.addEventListener('input', function() {
-        bg.volume = backgroundMusic.value;
-        setVolumeCookie(); // 볼륨 변경 시 쿠키 업데이트
-    });
-
-    // 효과음 볼륨 조절
-    soundEffects.addEventListener('input', function() {
-        se.volume = soundEffects.value;
-        setVolumeCookie(); // 볼륨 변경 시 쿠키 업데이트
-    });
 
     // 버튼 클릭 시 효과음 재생
     buttons.forEach(button => {
@@ -30,11 +16,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // 쿠키가 존재하는 경우, 볼륨 설정
     if (backgroundMusicVolume) {
-        backgroundMusic.value = backgroundMusicVolume;
         bg.volume = backgroundMusicVolume;
     }
     if (soundEffectsVolume) {
-        soundEffects.value = soundEffectsVolume;
         se.volume = soundEffectsVolume;
     }
 
@@ -43,15 +27,6 @@ document.addEventListener('DOMContentLoaded', function() {
 function playSoundEffect() {
     const sound = document.getElementById('se');
     sound.play();
-}
-
-function setVolumeCookie() {
-    const backgroundMusicVolume = backgroundMusic.value;
-    const soundEffectsVolume = soundEffects.value;
-
-    // 쿠키 이름과 값 설정, ${backgroundMusicVolume}는 변수 backgroundMusicVolume의 값
-    document.cookie = `backgroundMusicVolume=${backgroundMusicVolume}`;
-    document.cookie = `soundEffectsVolume=${soundEffectsVolume}`;
 }
 
 function getCookie(cookieName) {
